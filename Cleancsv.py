e918dbcf67a14989a4ade827959fa0cf5737c2d9
Jeremy_Chan_ThinkPad_X301 <jsc126@case.edu>
2017-04-13 21:03:09 -0400

    fully cleaned nyc data

--- Cleancsv.py
+++ Cleancsv.py
@@ -3,9 +3,29 @@ import numpy as np
 
 #returns dataframe storing cleaned up excel file
 def cleanNYC(file_name):
+    #col names available in file:
+    #vendor_id,pickup_datetime,dropoff_datetime,passenger_count,trip_distance,
+    #pickup_longitude,pickup_latitude,rate_code,store_and_fwd_flag,
+    #dropoff_longitude,dropoff_latitude,payment_type,fare_amount,surcharge,
+    #mta_tax,tip_amount,tolls_amount,total_amount
     df=pd.read_csv(file_name);
     df.drop(df.columns[[0,3,5,6,7,8,9,10,11]],axis=1)
-    #remove tip amount
+    #col names left:
+    #pickup_datetime,dropoff_datetime,trip_distance,
+    #fare_amount,surcharge,
+    #mta_tax,tip_amount,tolls_amount,total_amount
+    for x in xrange(len(df)):
+        tipamt=df.at[x,'tip_amount']
+        totaltemp=df.at[x,'total_amount']
+        df.at[x,'total_amount']=totaltemp-tipamt
+        totaltemp=df.at[x,'total_amount']
+        tollamt=df.at[x,'tolls_amount']
+        df.at[x,'total_amount']=totaltemp-tollamt
+    del df['tip_amount']
+    del df['tolls_amount']
+    del df['mta_tax']
+    del df['fare_amount']
+    del df['surcharge']
     return df
 
 def cleanUber(file_name):
@@ -24,15 +44,4 @@ cleanNYC('yellow_tripdata_2015-04.csv').to_pickle('nyc13.pkl')
 cleanNYC('yellow_tripdata_2015-05.csv').to_pickle('nyc14.pkl')
 cleanNYC('yellow_tripdata_2015-06.csv').to_pickle('nyc15.pkl')
 
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
-cleanUber
+
