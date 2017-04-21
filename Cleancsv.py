9c859ed1f2ab2a7fcae94c71a4000e288e2548e2
Jeremy_Chan_ThinkPad_X301 <jsc126@case.edu>
2017-04-20 23:50:18 -0400

    stashing

--- Cleancsv.py
+++ Cleancsv.py
@@ -8,7 +8,8 @@ def cleanNYC(file_name):
     #pickup_longitude,pickup_latitude,rate_code,store_and_fwd_flag,
     #dropoff_longitude,dropoff_latitude,payment_type,fare_amount,surcharge,
     #mta_tax,tip_amount,tolls_amount,total_amount
-    df=pd.read_csv(file_name);
+    df2=pd.read_pickle(file_name)
+    df=df2[:100000]
     df.drop(df.columns[[0,3,5,6,7,8,9,10,11]],axis=1)
     #col names left:
     #pickup_datetime,dropoff_datetime,trip_distance,
@@ -26,22 +27,18 @@ def cleanNYC(file_name):
     del df['mta_tax']
     del df['fare_amount']
     del df['surcharge']
-    return df
+    return df;
 
 def cleanUber(file_name):
-    df=pd.read_csv(file_name);
+    df=pd.read_pickle(file_name)
 
-cleanNYC('yellow_tripdata_2014-04.csv').to_pickle('nyc4.pkl')
-cleanNYC('yellow_tripdata_2014-05.csv').to_pickle('nyc5.pkl')
-cleanNYC('yellow_tripdata_2014-06.csv').to_pickle('nyc6.pkl')
-cleanNYC('yellow_tripdata_2014-07.csv').to_pickle('nyc7.pkl')
-cleanNYC('yellow_tripdata_2014-08.csv').to_pickle('nyc8.pkl')
-cleanNYC('yellow_tripdata_2014-09.csv').to_pickle('nyc9.pkl')
-cleanNYC('yellow_tripdata_2015-01.csv').to_pickle('nyc10.pkl')
-cleanNYC('yellow_tripdata_2015-02.csv').to_pickle('nyc11.pkl')
-cleanNYC('yellow_tripdata_2015-03.csv').to_pickle('nyc12.pkl')
-cleanNYC('yellow_tripdata_2015-04.csv').to_pickle('nyc13.pkl')
-cleanNYC('yellow_tripdata_2015-05.csv').to_pickle('nyc14.pkl')
-cleanNYC('yellow_tripdata_2015-06.csv').to_pickle('nyc15.pkl')
+nyc1=cleanNYC('nyc1.pkl')
+nyc2=cleanNYC('nyc2.pkl')
+nyc3=cleanNYC('nyc3.pkl')
+nyc4=cleanNYC('nyc4.pkl')
+uber1=cleanUber('uber1.pkl')
+uber2=cleanUber('uber2.pkl')
+uber3=cleanUber('uber3.pkl')
+uber4=cleanUber('uber4.pkl')
 
 
