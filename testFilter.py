74cc4e790a74f87c35274eda66e03042d12547d8
Jeremy <jsc126@case.edu>
2017-04-25 02:14:25 -0400

    finished filtering

--- testFilter.py
+++ testFilter.py
@@ -7,20 +7,46 @@ def filterTest(filename,weekend):
     #weekday and weekend are the actual price per mile
     columns=['weekday_price','weekday_distance','weekday','weekend_price','weekend_dist','weekend']
     filteredDf=pd.DataFrame(index=index,columns=columns)
-    weekendlistIndex=0
+    filteredDf[:]=0.0
+    currentDate=01
+    weekendIndex=0
+    isWeekend=False
+    #filling in values for the filtered dataframe
     for x in xrange(len(df)):
         tipamt=df.iloc[x]['tip_amount']
         totaltemp=df.iloc[x]['total_amount']
         df.loc[x,'total_amount']=totaltemp-tipamt
-        pickup_datetime=df.iloc[x]['pickup_datetime']
-        pickup_datetime=pickup_datetime[11:13]
+        pickup_time=df.iloc[x]['pickup_datetime']
+        pickup_time=pickup_time[11:13]
         pickup_date=df.iloc[x]['pickup_datetime']
         pickup_date=pickup_date[8:10]
-        #figure out how to do determine if weekend
-        filteredDf.loc[pickup_datetime]['weekday_price']+=totaltemp-tipamt
-    return filteredDf
-    
+        
+        if pickup_date!=currentDate:
+            currentDate=pickup_date
 
-weekendList=[4,5,11,12,18,19,25,26]
+        for y in xrange(len(weekend)):
+            if currentDate==weekend[y]:
+                isWeekend=True
+            
+        distance=df.iloc[x]['trip_distance']
+        
+        if isWeekend==False:
+            filteredDf.loc[pickup_time]['weekday_price']+=totaltemp-tipamt
+            filteredDf.loc[pickup_time]['weekday_distance']+=distance
+            
+        if isWeekend==True:
+            filteredDf.loc[pickup_time]['weekend_price']+=totaltemp-tipamt
+            filteredDf.loc[pickup_time]['weekend_dist']+=distance
+            
+        if filteredDf.loc[pickup_time]['weekday_price']!=0 and filteredDf.loc[pickup_time]['weekday_distance']!=0:
+            filteredDf.loc[pickup_time]['weekday']=filteredDf.loc[pickup_time]['weekday_price']/filteredDf.loc[pickup_time]['weekday_distance']
+        if filteredDf.loc[pickup_time]['weekend_price']!=0 and filteredDf.loc[pickup_time]['weekend_price']!=0:
+            filteredDf.loc[pickup_time]['weekend']=filteredDf.loc[pickup_time]['weekend_price']/filteredDf.loc[pickup_time]['weekend_dist']
+
+        isWeekend=False
+    return filteredDf
+
+weekendList=['04','05','11','12','18','19','25','26']
 df2=filterTest('test.csv',weekendList)
 print df2
+df2.to_csv('filtered.csv')
