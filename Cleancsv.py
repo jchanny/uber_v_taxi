ce0996f90371d92b4ebe209f247e3476cc99f849
Jeremy_Chan_ThinkPad_X301 <jsc126@case.edu>
2017-04-25 11:37:07 -0400

    filtering completo

--- Cleancsv.py
+++ Cleancsv.py
@@ -1,29 +1,67 @@
 import pandas as pd
 import numpy as np
 
-#creating is weekend column
-    #col names left:
-    #pickup_datetime,dropoff_datetime,trip_distance,
-    #fare_amount,surcharge,
-    #mta_tax,tip_amount,tolls_amount,total_amount
-#weekend is an array storing the dates of weekends in that month
-#newfile=filename for the filtered df
-def CleanNYC(file_name,weekend,newfile):
-    df.drop(df.columns[[0,3,5,6,7,8,9,10,11]],inplace='true',axis=1)
-    index=pd.Series(['1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am'])
-    columns=['Weekday price','Weekday distance','Weekday','Weekend price','Weekend distance','Weekend']
+def filterNYC(filename,weekend):
+    df=pd.read_csv(filename)
+    index=pd.Series(['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','00'])
+    #weekday and weekend are the actual price per mile
+    columns=['weekday_price','weekday_distance','weekday','weekend_price','weekend_dist','weekend']
     filteredDf=pd.DataFrame(index=index,columns=columns)
+    filteredDf[:]=0.0
+    currentDate=01
+    weekendIndex=0
+    isWeekend=False
+    #filling in values for the filtered dataframe
+    for x in xrange(len(df)):
+        tipamt=df.iloc[x]['tip_amount']
+        totaltemp=df.iloc[x]['total_amount']
+        df.loc[x,'total_amount']=totaltemp-tipamt
+        pickup_time=df.iloc[x]['pickup_datetime']
+        pickup_time=pickup_time[11:13]
+        pickup_date=df.iloc[x]['pickup_datetime']
+        pickup_date=pickup_date[8:10]
         
-        #code for filtered df
-        #figure out time stuff
+        if pickup_date!=currentDate:
+            currentDate=pickup_date
+
+        for y in xrange(len(weekend)):
+            if currentDate==weekend[y]:
+                isWeekend=True
+            
+        distance=df.iloc[x]['trip_distance']
         
+        if isWeekend==False:
+            filteredDf.loc[pickup_time]['weekday_price']+=totaltemp-tipamt
+            filteredDf.loc[pickup_time]['weekday_distance']+=distance
+            
+        if isWeekend==True:
+            filteredDf.loc[pickup_time]['weekend_price']+=totaltemp-tipamt
+            filteredDf.loc[pickup_time]['weekend_dist']+=distance
+            
+        if filteredDf.loc[pickup_time]['weekday_price']!=0 and filteredDf.loc[pickup_time]['weekday_distance']!=0:
+            filteredDf.loc[pickup_time]['weekday']=filteredDf.loc[pickup_time]['weekday_price']/filteredDf.loc[pickup_time]['weekday_distance']
+        if filteredDf.loc[pickup_time]['weekend_price']!=0 and filteredDf.loc[pickup_time]['weekend_price']!=0:
+            filteredDf.loc[pickup_time]['weekend']=filteredDf.loc[pickup_time]['weekend_price']/filteredDf.loc[pickup_time]['weekend_dist']
 
-    del df
-    filteredDf.to_hdf(newfile,'data',mode=w,format='table')
-    del filteredDf
-    
+        isWeekend=False
+    return filteredDf
 
+OctoberWeekendList=['04','05','11','12','18','19','25','26']
+nyc1=filterNYC('yellow_tripdata_2014-10.csv',OctoberWeekendList)
+nyc1.to_csv('nyc1.csv')
+del nyc1
 
--to do: get date format
-iterate through the df, determine if that current date is a weekend
-get pickup and dropoff time (lets use pickup time for time)
+NovemeberWeekendList=['1','2','8','9','15','16','22','23','29','30']
+nyc2=filterNYC('yellow_tripdata_2014-11.csv',NovemeberWeekendList)
+nyc2.to_csv('nyc2.csv')
+del nyc2
+
+DecemberWeekendList=['6','7','13','14','20','21','27','28']
+nyc3=filterNYC('yellow_tripdata_2014-12.csv',DecemberWeekendList)
+nyc3.to_csv('nyc3.csv')
+del nyc3
+
+JanuaryWeekendList=['3','4','10','11','17','18','24','25','31']
+nyc4=filterNYC('yellow_tripdata-2015-01.csv',JanuaryWeekendList)
+nyc4.to_csv('nyc4.csv')
+del nyc4
